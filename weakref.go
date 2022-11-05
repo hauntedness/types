5aae06a0a327bb0ed618f44f5afdfba901b93e27
huantedness <keikoamo@163.com>
2022-11-05 21:41:32 +0800

    remove types

--- weakref.go
+++ weakref.go
@@ -8,57 +8,54 @@ import (
 //
 //	WeakRef[T] get rid of garbage collector but then could not benefit from go type system!
 //	use this carefully
-type WeakRef[T any] struct {
+type WeakRef struct {
 	arena    *Arena
 	offset   int
 	pointers *[]unsafe.Pointer
 }
 
 // NewWeakRef make weak reference of T on [Arena].
-func NewWeakRef[T any](arena *Arena) WeakRef[T] {
-	offset := arena.Alloc(int(unsafe.Sizeof(*(*T)(nil))))
+func NewWeakRef(arena *Arena, size int) WeakRef {
+	offset := arena.Alloc(size)
 	p := make([]unsafe.Pointer, 0)
-	return WeakRef[T]{arena: arena, offset: offset, pointers: &p}
+	return WeakRef{arena: arena, offset: offset, pointers: &p}
 }
 
-func (wr WeakRef[T]) Size() int {
-	return int(unsafe.Sizeof(*(*T)(nil)))
-}
-
-func (wr WeakRef[T]) Offset() int {
+func (wr WeakRef) Offset() int {
 	return wr.offset
 }
 
-func (wr WeakRef[T]) MustSetInt(i int, v int) {
+func (wr WeakRef) MustSetInt(i int, v int) {
 	wr.arena.data[wr.offset+i] = v
 }
 
-func (wr WeakRef[T]) MustGetInt(i int) int {
+func (wr WeakRef) MustGetInt(i int) int {
 	return wr.arena.data[wr.offset+i]
 }
 
-// use pointer receiver is not recommend as the
-func (wr WeakRef[T]) MustSetString(i int, v string) {
+func (wr WeakRef) MustSetBytes(i int, v []byte) {
 	wr.MustSetPointer(i, unsafe.Pointer(&v))
 }
 
-func (wr WeakRef[T]) MustGetString(i int) string {
+func (wr WeakRef) MustGetBytes(i int) []byte {
+	return *(*[]byte)(wr.MustGetPointer(i))
+}
+
+// set pointer string or []byte is not recommend
+// as the native way is better choice
+func (wr WeakRef) MustSetString(i int, v string) {
+	wr.MustSetPointer(i, unsafe.Pointer(&v))
+}
+
+func (wr WeakRef) MustGetString(i int) string {
 	return *(*string)(wr.MustGetPointer(i))
 }
 
-func (wr WeakRef[T]) MustSetPointer(i int, ptr unsafe.Pointer) {
+func (wr WeakRef) MustSetPointer(i int, ptr unsafe.Pointer) {
 	wr.arena.data[wr.offset+i] = len(*wr.pointers)
 	*wr.pointers = append(*wr.pointers, ptr)
 }
 
-func (wr WeakRef[T]) MustGetPointer(i int) unsafe.Pointer {
+func (wr WeakRef) MustGetPointer(i int) unsafe.Pointer {
 	return (*wr.pointers)[wr.arena.data[wr.offset+i]]
-}
-
-func (wr WeakRef[T]) MustSetBytes(i int, v []byte) {
-	wr.MustSetPointer(i, unsafe.Pointer(&v))
-}
-
-func (wr WeakRef[T]) MustGetSlice(i int) []byte {
-	return *(*[]byte)(wr.MustGetPointer(i))
 }
--- weakref_test.go
+++ weakref_test.go
@@ -2,37 +2,41 @@ package types
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"unsafe"
 )
 
 func TestWeakRef_Offset(t *testing.T) {
-	var layout Layout
 	arena := NewArena()
-	wr := NewWeakRef[Layout](arena)
-	if int(unsafe.Sizeof(layout)) != wr.Size() {
-		t.Error("weak reference has wrong size")
-	}
-	wr2 := NewWeakRef[Layout](arena)
+	wr := NewWeakRef(arena, int(unsafe.Sizeof(*(*Layout)(nil))))
+	wr2 := NewWeakRef(arena, int(unsafe.Sizeof(*(*Layout)(nil))))
 	fmt.Println(wr2.Offset())
 	// text := "hello world"
 	wr.MustSetInt(0, 1)
 	fmt.Println(arena.data)
 }
 
+type SomeStruct struct {
+	Ptr *int
+	Num int
+}
+
+type Layout struct {
+	SomeText string
+	SomeInt  int
+	SomeBool bool
+}
+
 func TestWeakRef_Pointer(t *testing.T) {
-	type SomeStruct struct {
-		Ptr *int
-		Num int
-	}
 	arena := NewArena()
-	wr := NewWeakRef[SomeStruct](arena)
+	wr := NewWeakRef(arena, int(unsafe.Sizeof(*(*SomeStruct)(nil))))
 	var i int = 100
 	wr.MustSetPointer(0, unsafe.Pointer(&i))
 	ret := wr.MustGetPointer(0)
 	fmt.Println(*(*int)(ret))
 
-	wr2 := NewWeakRef[SomeStruct](arena)
+	wr2 := NewWeakRef(arena, int(unsafe.Sizeof(*(*SomeStruct)(nil))))
 	wr2.MustSetPointer(0, unsafe.Pointer(&i))
 	ret2 := wr2.MustGetPointer(0)
 	fmt.Println(*(*int)(ret2))
@@ -40,7 +44,7 @@ func TestWeakRef_Pointer(t *testing.T) {
 
 func TestWeakRef_String(t *testing.T) {
 	arena := NewArena()
-	wr := NewWeakRef[Layout](arena)
+	wr := NewWeakRef(arena, int(unsafe.Sizeof(*(*Layout)(nil))))
 	wr.MustSetString(0, "123")
 	ret := wr.MustGetString(0)
 	if ret != "123" {
@@ -48,8 +52,13 @@ func TestWeakRef_String(t *testing.T) {
 	}
 }
 
-type Layout struct {
-	SomeText string
-	SomeInt  int
-	SomeBool bool
+func TestWeakRef_Bytes(t *testing.T) {
+	arena := NewArena()
+	wr := NewWeakRef(arena, int(unsafe.Sizeof(*(*Layout)(nil))))
+	bytes := []byte("123")
+	wr.MustSetBytes(0, bytes)
+	ret := wr.MustGetBytes(0)
+	if !reflect.DeepEqual(bytes, ret) {
+		t.Error("want 123")
+	}
 }
